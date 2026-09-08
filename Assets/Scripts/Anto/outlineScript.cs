9b527fb635d0c729280fbf9a797a5218980a977f
Paille Antonin <a.paille@student.isartdigital.com>
2026-09-08 17:32:55 +0200

    [FIX] Pickup System

--- Assets/Scripts/Anto/outlineScript.cs
+++ Assets/Scripts/Anto/outlineScript.cs
@@ -7,41 +7,39 @@ public class OutlineOnHover : MonoBehaviour
     public Material outlineMaterial;
 
     private MeshRenderer meshRenderer;
-    private Material[] baseMaterials;      // juste le/les material(s) d'origine
-    private Material[] materialsWithOutline; // base + outline
+    private Material[] baseMaterials;
+    private Material[] materialsWithOutline;
 
     private bool mouseEntered = false;
 
-    [SerializeField] private Transform cameraTransform;
+    private Transform cameraTransform;
     [SerializeField] private float interactRange = 3f;
-    [SerializeField] private LayerMask interactMask = ~0;
     [SerializeField] private LayerMask playerMask;
 
     void Awake()
     {
         meshRenderer = GetComponent<MeshRenderer>();
 
-        // Récupère les materials d'origine (instance propre à cet objet)
         baseMaterials = meshRenderer.materials;
 
-        // Prépare le tableau avec l'outline en plus
         materialsWithOutline = new Material[baseMaterials.Length + 1];
         baseMaterials.CopyTo(materialsWithOutline, 0);
         materialsWithOutline[materialsWithOutline.Length - 1] = outlineMaterial;
     }
 
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+    }
+
     private void Update()
     {
+        if (cameraTransform == null)
+            return;
+
         if (Physics.Raycast(cameraTransform.position, cameraTransform.forward, out RaycastHit hit, interactRange, ~playerMask))
         {
-            if (mouseEntered)
-            {
-                meshRenderer.materials = materialsWithOutline;
-            }
-            else
-            {
-                meshRenderer.materials = baseMaterials;
-            }
+            meshRenderer.materials = mouseEntered ? materialsWithOutline : baseMaterials;
         }
         else
         {
